9c815517065ff644adbf8a8a81f0b02651a0f95a
Kvn05 <kavanvaghela1205@gmail.com>
2024-04-10 14:07:20 +0530

    creating new file spacecase.java

---
+++ spacecase.java
@@ -0,0 +1,5 @@
+public class spacecase {
+    public static void main(String[] args) {
+        System.out.println("there is no any changing occur made since we are learning about github just");
+    }
+}
